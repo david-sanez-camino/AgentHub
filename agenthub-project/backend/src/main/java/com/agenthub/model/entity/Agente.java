ba576f653e07402033966952d8eb68e1d81635a7
Miguel Ramirez <miguelramirez2210@gmail.com>
2026-03-01 12:52:24 +0100

    feat: Actualicé la entidad de agente con relacion a Desarrollador

--- agenthub-project/backend/src/main/java/com/agenthub/model/entity/Agente.java
+++ agenthub-project/backend/src/main/java/com/agenthub/model/entity/Agente.java
@@ -11,6 +11,11 @@ public class Agente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    // Conexion de cada agente que se crea con el desarrollador 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Desarrollador desarrollador;
+
     private String nombre;
     private String descripcion;
     private String systemPromt; //------ CORREGIR EN LA BD --------- PROMPT****
@@ -22,8 +27,16 @@ public class Agente {
     @Builder.Default
     private Boolean publicado = false;
 
+    //  Control para el admin que aprueba el agente antes de salir al marketplace 
+    @Column(name = "estado_verificacion", nullable = false)
+    @Builder.Default
+    private String estadoVerificacion = "PENDIENTE"; // valores posibles: pendiente, aprobado, rechazado
+
     // relacion n->n con entidad Herramienta 
     @ManyToMany
-    @JoinTable(name = "agente_herramienta", joinColumns = @JoinColumn(name = "id_agente"), inverseJoinColumns = @JoinColumn(name = "id_herramienta"))
+    @JoinTable(name = "agente_herramienta", 
+        joinColumns = @JoinColumn(name = "id_agente"), 
+        inverseJoinColumns = @JoinColumn(name = "id_herramienta")
+    )
     private Set<Herramienta> herramientas;
 }
